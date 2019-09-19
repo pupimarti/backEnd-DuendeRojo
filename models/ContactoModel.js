62298740ebe5a3094637f8540fd5dabb33213a7b
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2019-09-19 14:34:49 -0300

    Add numero

--- models/ContactoModel.js
+++ models/ContactoModel.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var contactoSchema = new Schema({
-    cNombre: String,
+      numero: String,
+      cNombre: String,
       cApellido: String,
       cSexo: String,
       cFecha: String,
@@ -16,9 +17,15 @@ var contactoSchema = new Schema({
       pTel: String,
       pCel: String,
       pWhapp: String,
+      cCeliaco: String,
+      cRespiratoria: String,
+      cERespiratoria: String,
+      cCorazon: String,
+      cECorazon: String,
+      cHeridas: String,
+      cEHeridas: String,
       pagado: String
 });
 
 var Colonos = mongoose.model('Colono', contactoSchema);
-console.log("se creo modelo");
 module.exports = Colonos;
